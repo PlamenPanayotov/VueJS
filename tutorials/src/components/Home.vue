95c9ba7bb2929a2da764f9c3fa69e07e4a625171
PlamenPanayotov <p.panayotov@mail.bg>
2020-04-29 08:20:50 +0300

    workshop

---
+++ tutorials/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <app-content>
+    <template v-slot:nav>
+      <ul>
+        <li>
+          <a href="#">JS Conditional Statements</a>
+        </li>
+        <li>
+          <a href="#">JS Loops</a>
+        </li>
+        <li>
+          <a href="#">JS Arrays</a>
+        </li>
+        <li>
+          <a href="#">JS Objects</a>
+        </li>
+      </ul>
+    </template>
+  </app-content>
+</template>
+
+<script>
+import AppContent from "./shared/Content.vue";
+export default {
+  components: {
+    AppContent,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ tutorials/src/components/shared/Content.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="navigation">
+      <ul>
+        <li>
+          <a href="#">C#</a>
+        </li>
+        <li>
+          <a href="#">JAVASCRIPT</a>
+        </li>
+        <li>
+          <a href="#">JAVA</a>
+        </li>
+        <li>
+          <a href="#">PYTHON</a>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <a href="#">SUBJECT [+]</a>
+        </li>
+      </ul>
+    </div>
+    <div class="main-content">
+      <aside class="content-navigation">
+          <slot name="nav"></slot>
+        
+      </aside>
+      <section class="content-info">
+          <slot name="info"></slot>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
